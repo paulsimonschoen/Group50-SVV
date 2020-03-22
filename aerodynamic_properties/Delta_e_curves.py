04737b77bd6bd32c5d177f71720d1cfb796f3de7
FD95 <61151330+FD95@users.noreply.github.com>
2020-03-22 18:48:18 +0100

    Add files via upload

--- aerodynamic_properties/Delta_e_curves.py
+++ aerodynamic_properties/Delta_e_curves.py
@@ -39,6 +39,8 @@ trim_curve_data = pd.read_excel("Trim_curve_data.xlsx")         #Import Excel Fi
 trim_curve_data.columns = ['Altitude_ft', 'IAS', 'a', 'de', 'detr', 'Fe', 'FFL_lbs/hr', 'FFR_lbs/hr', 'F_used', 'TAT_C']
 seat_data = pd.read_excel("Seat_data.xlsx")         #Import Excel File with all the data
 seat_data.columns = ['Seat_1', 'Seat_2', 'W_pass']
+cg_data = pd.read_excel("cg_data.xlsx")
+cg_data.columns = ['Altitude_ft', 'IAS', 'a', 'de', 'detr', 'Fe', 'FFL_lbs/hr', 'FFR_lbs/hr', 'F_used', 'TAT_C']
 thrust_input_data = pd.read_excel("Thrust_input_data.xlsx")         #Import Excel File with all the data
 thrust_input_data.columns = ['T_l', 'T_r']
 
@@ -54,10 +56,10 @@ ffr = np.asarray(trim_curve_data['FFR_lbs/hr']*0.45359237/3600) #Fuel flow right
 F_used = np.asarray(trim_curve_data['F_used']*0.45359237)
 T_tot_K = np.asarray(trim_curve_data['TAT_C'] + K)              #Total Temperature in [K]
 d_T = T_tot_K-(T_0 + dT_h*altitude_m)                           #Change in Temp in [K/m]
-W_pass_kg = np.asarray(seat_data['W_pass'])                     #Passenger weight in [kg]
+W_pass = np.asarray(seat_data['W_pass'])                     #Passenger weight in [kg]
 seat1 = np.asarray(seat_data['Seat_1']*0.0254)
 seat2 = np.asarray(seat_data['Seat_2']*0.0254)
-W_tot = ((W_f + W_emp)*0.45359237 + np.sum(W_pass_kg))*g
+W_tot = ((W_f + W_emp)*0.45359237 + np.sum(W_pass))*g
 
 #Calculations
 p_2 = p_0 * ((1+(dT_h*altitude_m/T_0))**(-g/(dT_h*R)))
@@ -72,6 +74,7 @@ V_re2 = V_e2*np.sqrt(Ws/W_2)
 F_es = F_e *(Ws/W_2)
 
 #Plots
+#de/Vre-plot with regression line
 poly_fit_d = np.poly1d(np.polyfit(V_re2, delta_e, 2))
 plt.figure(1)
 xx = np.linspace(np.amin(V_re2), np.amax(V_re2), 100)
@@ -79,10 +82,16 @@ plt.plot(xx, poly_fit_d(xx), c='r',linestyle='-')
 plt.scatter(V_re2, delta_e)
 plt.show()
 
-
+#Fe/Vre-plot with regression line
 poly_fit_f = np.poly1d(np.polyfit(V_re2, F_es, 2))
 plt.figure(2)
 xx = np.linspace(np.amin(V_re2), np.amax(V_re2), 100)
 plt.plot(xx, poly_fit_f(xx), c='r',linestyle='-')
 plt.scatter(V_re2, F_es)
+plt.show()
+
+poly_fit_da = np.poly1d(np.polyfit(alpha, delta_e, 1))
+print("Linear equation d_delta/d_alpha =", poly_fit_da)
+plt.figure(3)
+plt.scatter(alpha, delta_e)
 plt.show()
