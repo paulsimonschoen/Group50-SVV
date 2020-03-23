1daa9e8fc3de19b6fe82d4baf0f504c2c272abff
Paul Simon Schoen <p.s.schon@student.tudelft.nl>
2020-03-23 21:22:40 +0100

    Statespace System Calc

---
+++ StateSpaceSystem.py
@@ -0,0 +1,39 @@
+import control.matlab as ml
+import matplotlib.pyplot as plt
+import scipy as np
+
+
+class State_Space:
+    def __init__(self,A,B,x0):
+        self.A = A
+        self.B = B
+        self.C = np.identity(4)
+        self.D = np.zeros(1,4)
+        self.x0 = x0
+
+    def get_ss(self):
+        return ml.ss(A,B,C,D)
+
+    def get_eig(self):
+        return np.eig(A)
+
+    def get_response(self,u):
+        ss = self.get_ss()
+        t = np.arange(u)
+        return ml.lsim(ss,u,t,self.x0),t
+
+    def plot_resp(self,u):
+        fig = plt.figure()
+        resp,t = self.get_response(u)
+        plt.plot(resp,t)
+        plt.show()
+
+
+
+
+
+
+
+
+
+
